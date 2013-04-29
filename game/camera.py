4ffb7cba00a3d9852613a97a6be6e17347618a9a
Adam Kidder <thekidder@gmail.com>
2013-04-28 21:15:47 -0700

    camera class

---
+++ game/camera.py
@@ -0,0 +1,15 @@
+from kidgine.math.vector import Vector
+import kidgine.renderer.camera
+
+
+class VerticalPanningCamera(kidgine.renderer.camera.CenteredCamera):
+    def __init__(self, anchor, center_x, width):
+        self._anchor = anchor
+        self._x = center_x
+        self._last = 0
+        def anchor_fn():
+            if self._anchor is not None:
+                self._last = self._anchor.position.y
+            return Vector(self._x, self._last)
+
+        super(VerticalPanningCamera, self).__init__(anchor_fn, Vector(width))
