0aab7451985d40dce96408db3f89df5e80229655
Adam Kidder <thekidder@gmail.com>
2013-04-28 18:41:11 -0700

    removed test assets

--- data/tilesets.py
+++ data/tilesets.py
@@ -1,5 +1,4 @@
 all = {
-    'data/images/test_tileset.png': set([2, 3, 121, 122, 123, 124, 252]),
     'data/images/tilesets/castle-tile-set.png': set([]),
     'data/images/tilesets/desert-tile-set.png': set([1,2,3,4,5,6,7,8])#,37,39]) #cacti
 }
